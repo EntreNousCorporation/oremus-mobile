7771933a7a0f9b847be62b96595ae30a7411795d
ino <innocent.nguessan@smile.ci>
2022-03-19 15:34:23 +0000

    add carousel

--- lib/app/modules/paroisse/views/paroisse_screen.dart
+++ lib/app/modules/paroisse/views/paroisse_screen.dart
@@ -1,22 +1,23 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
-import 'package:oremusapp/app/commons/components/app_navigation_bar.dart';
 import 'package:oremusapp/app/commons/theme/app_colors.dart';
 import 'package:oremusapp/app/commons/theme/app_text_theme.dart';
 import 'package:oremusapp/app/modules/paroisse/controller/paroisse_controller.dart';
+import 'package:oremusapp/app/modules/paroisse/views/widget/image_slider.dart';
 import 'package:oremusapp/app/modules/paroisse/views/widget/search_widget.dart';
+import 'package:oremusapp/app/modules/paroisse/views/widget/slider_indicator.dart';
 
 class ParoisseScreen extends StatelessWidget {
   const ParoisseScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
       color: colorGreen,
       child: SafeArea(
-        child: GetBuilder<ParoisseController>(
+        child: GetX<ParoisseController>(
             initState: (state) {},
             builder: (_) {
               return WillPopScope(
@@ -26,61 +27,83 @@ class ParoisseScreen extends StatelessWidget {
                     resizeToAvoidBottomInset: true,
                     body: Container(
                       color: colorGrey4,
-                      padding: const EdgeInsets.only(
-                          top: 24, bottom: 0, left: 16, right: 16),
+                      //padding: const EdgeInsets.only(top: 24, bottom: 0, left: 16, right: 16),
                       child: Column(
                         children: [
-                          AppNavigationBar(
-                            title: 'Accueil',
-                            showBackButton: false,
+                          Stack(
+                            children: [
+                              SizedBox(
+                                width: Get.width,
+                                child: CarouselSlider.builder(
+                                  carouselController: _.carouselController,
+                                  itemCount: _.imgList.length,
+                                  itemBuilder: (BuildContext context,
+                                      int itemIndex, int pageViewIndex) {
+                                    final image = _.imgList[itemIndex];
+                                    return ImageSlider(image: image);
+                                  },
+                                  options: _.carouselOptions,
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0.0,
+                                left: 0.0,
+                                right: 0.0,
+                                child: SliderIndicator(controller: _),
+                              ),
+                            ],
                           ),
                           Expanded(
-                            child: SingleChildScrollView(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 0),
-                                child: Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      height: (Get.width / 9),
-                                      child: const SearchWidget(),
-                                    ),
-                                    Separators.maximumVertical(),
-                                    GridView.builder(
-                                      physics:
-                                          const NeverScrollableScrollPhysics(),
-                                      shrinkWrap: true,
-                                      gridDelegate:
-                                          const SliverGridDelegateWithFixedCrossAxisCount(
-                                            childAspectRatio: 3/2,
-                                            crossAxisCount: 2,
-                                            crossAxisSpacing: 16.0,
-                                            mainAxisSpacing: 16.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 24, bottom: 0, left: 16, right: 16),
+                              child: SingleChildScrollView(
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(
+                                        height: (Get.width / 9),
+                                        child: const SearchWidget(),
                                       ),
-                                      itemCount: _.operations.length,
-                                      itemBuilder: (context, index) {
-                                        var operation = _.operations[index];
-                                        return Material(
-                                          borderRadius:
-                                              BorderRadius.circular(8.0),
-                                          elevation: 0,
-                                          child: Center(
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                const Icon(Icons.search),
-                                                Separators.normalVertical(),
-                                                const Text('Localisation'),
-                                              ],
+                                      Separators.maximumVertical(),
+                                      GridView.builder(
+                                        physics:
+                                            const NeverScrollableScrollPhysics(),
+                                        shrinkWrap: true,
+                                        gridDelegate:
+                                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                          childAspectRatio: 3 / 2,
+                                          crossAxisCount: 2,
+                                          crossAxisSpacing: 16.0,
+                                          mainAxisSpacing: 16.0,
+                                        ),
+                                        itemCount: _.operations.length,
+                                        itemBuilder: (context, index) {
+                                          var operation = _.operations[index];
+                                          return Material(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                            elevation: 0,
+                                            child: Center(
+                                              child: Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
+                                                  const Icon(Icons.search),
+                                                  Separators.normalVertical(),
+                                                  const Text('Localisation'),
+                                                ],
+                                              ),
                                             ),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ],
+                                          );
+                                        },
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
---
+++ lib/app/modules/paroisse/views/widget/image_slider.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ImageSlider extends StatelessWidget {
+  const ImageSlider({Key? key, this.image}) : super(key: key);
+
+  final image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(0.0),
+      child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(0.0)),
+          child: Stack(
+            children: <Widget>[
+              Image.network(image, fit: BoxFit.cover, width: Get.width),
+              Visibility(
+                visible: false,
+                child: Positioned(
+                  bottom: 0.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(200, 0, 0, 0),
+                          Color.fromARGB(0, 0, 0, 0)
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 20.0),
+                    child: const Text(
+                      'No. image',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
