e4ef802cdda8363481ff4693f3b4b433faa544ae
ino <innocent.nguessan@smile.ci>
2022-03-27 04:34:12 +0000

    add menu drawer

--- lib/app/modules/home/views/widget/menu_grid_item.dart
+++ lib/app/modules/home/views/widget/menu_grid_item.dart
@@ -15,36 +15,24 @@ class MenuGridItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Material(
-        borderRadius:
-        BorderRadius.circular(10.0),
+        borderRadius: BorderRadius.circular(10.0),
         elevation: 10,
         color: colorWhite,
-        shadowColor:
-        colorGrey2.withOpacity(0.5),
+        shadowColor: colorGrey2.withOpacity(0.5),
         child: Column(
-          mainAxisAlignment:
-          MainAxisAlignment.center,
-          crossAxisAlignment:
-          CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Image.asset('${item.icon}', height: Get.width / 8),
             Separators.minimunVertical(),
             Padding(
-              padding: const EdgeInsets
-                  .symmetric(
-                  horizontal: 8.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Text(
                 '${item.libelle}',
                 maxLines: 2,
-                overflow: TextOverflow
-                    .ellipsis,
-                style: TextStyles
-                    .montserratBold(
-                    textSize:
-                    TextSizes
-                        .thirteen,
-                    textColor:
-                    colorBlack),
+                overflow: TextOverflow.ellipsis,
+                style: TextStyles.montserratBold(
+                    textSize: TextSizes.thirteen, textColor: colorBlack),
               ),
             ),
           ],
