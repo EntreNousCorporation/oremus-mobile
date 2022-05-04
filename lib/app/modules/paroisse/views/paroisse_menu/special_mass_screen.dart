35b542faf719e88e8731025b387fc63a1559eccc
ino <innocent.nguessan@smile.ci>
2022-05-04 18:23:52 +0000

    reviews special mass time

--- lib/app/modules/paroisse/views/paroisse_menu/special_mass_screen.dart
+++ lib/app/modules/paroisse/views/paroisse_menu/special_mass_screen.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_fadein/flutter_fadein.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:grouped_list/grouped_list.dart';
 import 'package:oremusapp/app/commons/components/lottie_loader_widget.dart';
 import 'package:oremusapp/app/commons/components/not_found_page.dart';
 import 'package:oremusapp/app/commons/theme/app_colors.dart';
 import 'package:oremusapp/app/commons/theme/app_dimension.dart';
 import 'package:oremusapp/app/commons/theme/app_text_theme.dart';
 import 'package:oremusapp/app/modules/paroisse/controller/paroisse_menu/paroisse_masse_controller.dart';
+import 'package:oremusapp/app/modules/paroisse/data/model/liturgical_celebration_response.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class SpecialMassScreen extends StatelessWidget {
@@ -29,13 +31,44 @@ class SpecialMassScreen extends StatelessWidget {
               child: SmartRefresher(
                 controller: logic.refreshNotRecurrentController,
                 onRefresh: logic.onSpecialMassesRefresh,
-                child: ListView.builder(
-                  itemCount: logic.specialMasses.length,
-                  itemBuilder: (context, index) {
-                    var specialMass = logic.specialMasses[index];
-                    return ListTile(
-                      title: Text('${specialMass.name}'),
-                      subtitle: Text('${specialMass.startDate}'),
+                child: GroupedListView<LiturgicalCelebrationResponse?, String>(
+                  padding: const EdgeInsets.all(16.0),
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  elements: logic.specialMasses.value,
+                  useStickyGroupSeparators: false,
+                  groupBy: (liturgicalCelebration) =>
+                      liturgicalCelebration?.name ?? '',
+                  groupHeaderBuilder: (liturgicalCelebration) => Container(
+                    padding:
+                        const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: colorGreenSemiLight.withOpacity(0.4),
+                    ),
+                    child: Center(
+                      child: Text(liturgicalCelebration?.name ?? '',
+                        style: TextStyles.montserratBold(
+                          textSize: TextSizes.eighteen,
+                        ),),
+                    ),
+                  ),
+                  order: GroupedListOrder.ASC,
+                  itemBuilder: (c, liturgicalCelebration) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '${liturgicalCelebration?.startDate}',
+                            style: TextStyles.montserratMedium(
+                              textColor: colorBlack,
+                              textSize: TextSizes.thirteen,
+                            ),
+                          ),
+                        ),
+                      ],
                     );
                   },
                 ),
