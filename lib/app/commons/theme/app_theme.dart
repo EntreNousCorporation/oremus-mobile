32e2091b7103260fad89af26c8278da78cd98b7d
ino <amourssou@gmail.com>
2022-05-16 15:05:04 +0000

    update font

--- lib/app/commons/theme/app_theme.dart
+++ lib/app/commons/theme/app_theme.dart
@@ -3,14 +3,14 @@ import 'package:flutter/services.dart';
 
 import 'app_colors.dart';
 final ThemeData appThemeData = ThemeData(
-  appBarTheme: AppBarTheme(
+  appBarTheme: const AppBarTheme(
     systemOverlayStyle: SystemUiOverlayStyle.dark, // 2
   ),
   primaryColor: colorGreen,
   splashColor: colorWhite,
   highlightColor: colorGreen,
   primaryColorDark: colorGreen,
-  fontFamily: 'avenir_regular',
+  fontFamily: 'montserrat_regular',
  //brightness: Brightness.light,
  /* textTheme: TextTheme(
     headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
